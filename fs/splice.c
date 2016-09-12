679c0c0feb58491a596d8f04c61979fffb302d9f
Miklos Szeredi <mszeredi@redhat.com>
2016-09-12 23:23:10 +0200

    splice: don't check i_size in generic_file_splice_read()
    This is handled in get_page_for_read() already.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/splice.c
+++ fs/splice.c
@@ -334,19 +334,10 @@ ssize_t generic_file_splice_read(struct file *in, loff_t *ppos,
 				 struct pipe_inode_info *pipe, size_t len,
 				 unsigned int flags)
 {
-	loff_t isize, left;
 	int ret;
 
 	if (IS_DAX(in->f_mapping->host))
 		return default_file_splice_read(in, ppos, pipe, len, flags);
-
-	isize = i_size_read(in->f_mapping->host);
-	if (unlikely(*ppos >= isize))
-		return 0;
-
-	left = isize - *ppos;
-	if (unlikely(left < len))
-		len = left;
 
 	ret = __generic_file_splice_read(in, ppos, pipe, len, flags);
 	if (ret > 0) {
