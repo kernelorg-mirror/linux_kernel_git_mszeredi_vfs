4c6564a64ee8024096b34e6d2f4a4e0e4df41bf8
Miklos Szeredi <mszeredi@redhat.com>
2020-12-07 15:25:18 +0100

    ovl: do not fail because of O_NOATIME
    In case the file cannot be opened with O_NOATIME because of lack of
    capabilities, then clear O_NOATIME instead of failing.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/file.c
+++ fs/overlayfs/file.c
@@ -53,9 +53,10 @@ static struct file *ovl_open_realfile(const struct file *file,
 	err = inode_permission(realinode, MAY_OPEN | acc_mode);
 	if (err) {
 		realfile = ERR_PTR(err);
-	} else if (!inode_owner_or_capable(realinode)) {
-		realfile = ERR_PTR(-EPERM);
 	} else {
+		if (!inode_owner_or_capable(realinode))
+			flags &= ~O_NOATIME;
+
 		realfile = open_with_fake_path(&file->f_path, flags, realinode,
 					       current_cred());
 	}
