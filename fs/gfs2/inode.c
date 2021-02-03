36ebe96488153e73a8f8aea69a43a537751f7b94
Miklos Szeredi <mszeredi@redhat.com>
2021-02-03 10:41:33 +0100

    gfs2: convert to miscattr
    Use the miscattr API to let the VFS handle locking, permission checking and
    conversion.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Cc: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/inode.c
+++ fs/gfs2/inode.c
@@ -2145,6 +2145,8 @@ static const struct inode_operations gfs2_file_iops = {
 	.get_acl = gfs2_get_acl,
 	.set_acl = gfs2_set_acl,
 	.update_time = gfs2_update_time,
+	.miscattr_get = gfs2_miscattr_get,
+	.miscattr_set = gfs2_miscattr_set,
 };
 
 static const struct inode_operations gfs2_dir_iops = {
@@ -2166,6 +2168,8 @@ static const struct inode_operations gfs2_dir_iops = {
 	.set_acl = gfs2_set_acl,
 	.update_time = gfs2_update_time,
 	.atomic_open = gfs2_atomic_open,
+	.miscattr_get = gfs2_miscattr_get,
+	.miscattr_set = gfs2_miscattr_set,
 };
 
 static const struct inode_operations gfs2_symlink_iops = {
