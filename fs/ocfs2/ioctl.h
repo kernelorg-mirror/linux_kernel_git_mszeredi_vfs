6f0c46a418b8396f8a85787ecbed230db7d43a15
Miklos Szeredi <mszeredi@redhat.com>
2021-02-03 10:41:34 +0100

    ocfs2: convert to miscattr
    Use the miscattr API to let the VFS handle locking, permission checking and
    conversion.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Cc: Joel Becker <jlbec@evilplan.org>

--- fs/ocfs2/ioctl.h
+++ fs/ocfs2/ioctl.h
@@ -11,6 +11,8 @@
 #ifndef OCFS2_IOCTL_PROTO_H
 #define OCFS2_IOCTL_PROTO_H
 
+int ocfs2_miscattr_get(struct dentry *dentry, struct miscattr *ma);
+int ocfs2_miscattr_set(struct dentry *dentry, struct miscattr *ma);
 long ocfs2_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
 long ocfs2_compat_ioctl(struct file *file, unsigned cmd, unsigned long arg);
 
