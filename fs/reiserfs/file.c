2cd4e3b48f1d06ab7600bb7e5e9ca5e703dc54e8
Miklos Szeredi <mszeredi@redhat.com>
2021-02-03 10:41:34 +0100

    reiserfs: convert to miscattr
    Use the miscattr API to let the VFS handle locking, permission checking and
    conversion.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Cc: Jan Kara <jack@suse.cz>

--- fs/reiserfs/file.c
+++ fs/reiserfs/file.c
@@ -258,4 +258,6 @@ const struct inode_operations reiserfs_file_inode_operations = {
 	.permission = reiserfs_permission,
 	.get_acl = reiserfs_get_acl,
 	.set_acl = reiserfs_set_acl,
+	.miscattr_get = reiserfs_miscattr_get,
+	.miscattr_set = reiserfs_miscattr_set,
 };
