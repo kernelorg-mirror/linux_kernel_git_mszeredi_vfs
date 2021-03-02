cb4dd2930697291a73da6ab4f7e92fd25d32a86f
Miklos Szeredi <mszeredi@redhat.com>
2021-03-02 13:38:23 +0100

    ext4: convert to miscattr
    Use the miscattr API to let the VFS handle locking, permission checking and
    conversion.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Cc: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/file.c
+++ fs/ext4/file.c
@@ -919,5 +919,7 @@ const struct inode_operations ext4_file_inode_operations = {
 	.get_acl	= ext4_get_acl,
 	.set_acl	= ext4_set_acl,
 	.fiemap		= ext4_fiemap,
+	.miscattr_get	= ext4_miscattr_get,
+	.miscattr_set	= ext4_miscattr_set,
 };
 
--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -4144,6 +4144,8 @@ const struct inode_operations ext4_dir_inode_operations = {
 	.get_acl	= ext4_get_acl,
 	.set_acl	= ext4_set_acl,
 	.fiemap         = ext4_fiemap,
+	.miscattr_get	= ext4_miscattr_get,
+	.miscattr_set	= ext4_miscattr_set,
 };
 
 const struct inode_operations ext4_special_inode_operations = {
