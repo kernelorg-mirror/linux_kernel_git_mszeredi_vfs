9b5ff63d2b9b94298e6c9fbac4df9e77e6901fc9
Miklos Szeredi <mszeredi@redhat.com>
2023-09-20 10:34:09 +0200

    add listmount(2) syscall
    Add way to query the children of a particular mount.  This is a more
    flexible way to iterate the mount tree than having to parse the complete
    /proc/self/mountinfo.
    
    Lookup the mount by the new 64bit mount ID.  If a mount
    needs to be queried based on path, then statx(2) can be used to first query
    the mount ID belonging to the path.
    
    Return an array of new (64bit) mount ID's.  Without privileges only mounts
    are listed which are reachable from the task's root.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -412,6 +412,8 @@ asmlinkage long sys_fstatfs64(unsigned int fd, size_t sz,
 asmlinkage long sys_statmount(u64 mnt_id, u64 mask,
 			      struct statmnt __user *buf, size_t bufsize,
 			      unsigned int flags);
+asmlinkage long sys_listmount(u64 mnt_id, u64 __user *buf, size_t bufsize,
+			      unsigned int flags);
 asmlinkage long sys_truncate(const char __user *path, long length);
 asmlinkage long sys_ftruncate(unsigned int fd, unsigned long length);
 #if BITS_PER_LONG == 32
